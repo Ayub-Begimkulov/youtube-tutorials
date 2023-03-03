9a70c59f01c62fa62992ff8a4765f7399a39c828
Ayub-Begimkulov <begimkulov963@gmail.com>
2023-03-03 19:25:31 +0500

    [i18n] full example fix

--- how-i-do-i18n/src/pages/basic/i18n/setup.ts
+++ how-i-do-i18n/src/pages/basic/i18n/setup.ts
@@ -8,19 +8,6 @@ import {
 import { en } from "./keys/en";
 import { ru } from "./keys/ru";
 
-/* const timeout =
-    <T>(timeout: number) =>
-    (value: T) => {
-      return new Promise<T>((res) => {
-        setTimeout(() => res(value), timeout);
-      });
-    };
-  
-  const loadEn = () =>
-    import("./keys/en").then((module) => module.en).then(timeout(1_500));
-  const loadRu = () =>
-    import("./keys/ru").then((module) => module.ru).then(timeout(1_500)); */
-
 export const i18n = new I18N({
   defaultLang: "ru",
   languages: {
--- how-i-do-i18n/src/pages/full/i18n/setup.ts
+++ how-i-do-i18n/src/pages/full/i18n/setup.ts
@@ -5,18 +5,29 @@ import {
   useTranslate as useTranslateBase,
   useI18N as useI18NBase,
 } from "../../../i18n";
-import { en } from "./keys/en";
-import { ru } from "./keys/ru";
+
+const timeout =
+  <T>(timeout: number) =>
+  (value: T) => {
+    return new Promise<T>((res) => {
+      setTimeout(() => res(value), timeout);
+    });
+  };
+
+const loadEn = () =>
+  import("./keys/en").then((module) => module.en).then(timeout(1_500));
+const loadRu = () =>
+  import("./keys/ru").then((module) => module.ru).then(timeout(1_500));
 
 export const i18n = new I18N({
   defaultLang: "ru",
   languages: {
     en: {
-      keyset: en,
+      keyset: loadEn,
       pluralize: pluralizeEn,
     },
     ru: {
-      keyset: ru,
+      keyset: loadRu,
       pluralize: pluralizeRu,
     },
   },
