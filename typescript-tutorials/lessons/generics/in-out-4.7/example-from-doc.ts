29e6eea72a903b65fdd7e6b65d95958ccc339d78
Ayub-Begimkulov <begimkulov963@gmail.com>
2023-03-17 18:15:05 +0500

    generics tutorial

--- typescript-tutorials/lessons/generics/in-out-4.7/example-from-doc.ts
+++ typescript-tutorials/lessons/generics/in-out-4.7/example-from-doc.ts
@@ -1,10 +1,12 @@
 export {};
 
-type Foo<T> = {
+type Foo<in out T> = {
   x: T;
   f: Bar<T>;
 };
+
 type Bar<U> = (x: Baz<U>) => void;
+
 type Baz<V> = {
   value: Foo<V[]>;
 };
@@ -14,3 +16,10 @@ declare let foo2: Foo<string>;
 
 foo1 = foo2; // Should be an error but isn't ❌
 foo2 = foo1; // Error - correct ✅
+
+declare const a: boolean | string | number;
+
+if (a) {
+  a;
+  // ^?
+}
--- typescript-tutorials/lessons/generics/in-out-4.7/example.ts
+++ typescript-tutorials/lessons/generics/in-out-4.7/example.ts
@@ -8,5 +8,34 @@ class Specific extends Base {
   b = 10;
 }
 
-type Getter<T> = () => T;
-type Setter<T> = (value: T) => void;
+type Getter<out T> = () => T;
+type Setter<in T> = (value: T) => void;
+
+interface GetterAndSetter<in out T> {
+  get: () => T;
+  set: (value: T) => void;
+}
+
+type BaseGetter = Getter<Base>;
+type SpecificGetter = Getter<Specific>;
+
+declare let baseGetter: BaseGetter;
+declare let specificGetter: SpecificGetter;
+
+// type Test = Base extends Specific ? true : false
+
+baseGetter = specificGetter;
+specificGetter = baseGetter;
+
+// ============
+
+type BaseSetter = Setter<Base>;
+type SpecificSetter = Setter<Specific>;
+
+declare let baseSetter: BaseSetter;
+declare let specificSetter: SpecificSetter;
+
+// type Test = Specific extends Base ? true : false
+
+baseSetter = specificSetter;
+specificSetter = baseSetter;
