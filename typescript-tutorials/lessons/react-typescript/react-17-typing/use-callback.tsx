7570940db7d15d9806bf4d7771ccb77af4ab2b64
Ayub-Begimkulov <begimkulov963@gmail.com>
2023-03-29 10:57:39 +0500

    react typescript

--- typescript-tutorials/lessons/react-typescript/react-17-typing/use-callback.tsx
+++ typescript-tutorials/lessons/react-typescript/react-17-typing/use-callback.tsx
@@ -1,4 +1,4 @@
-import { DependencyList, useCallback } from "react";
+import { /* DependencyList, */ useCallback } from "react";
 
 const cb = useCallback((arg1, arg2, arg3) => {
   arg1;
@@ -11,11 +11,11 @@ const cb = useCallback((arg1, arg2, arg3) => {
 
 cb(1, 2, 3);
 
-function useCallback<T extends (...args: any[]) => any>(
-  callback: T,
-  deps: DependencyList
-): T;
+// function useCallback<T extends (...args: any[]) => any>(
+//   callback: T,
+//   deps: DependencyList
+// ): T;
 
-function useCallback(...args: any[]): any {
-  return "asdf";
-}
+// function useCallback(...args: any[]): any {
+//   return "asdf";
+// }
