e1a088895c6f1b02c50ba4fb0e8c6c84a8c6af24
Ayub-Begimkulov <begimkulov963@gmail.com>
2023-04-09 01:15:23 +0500

    use ref hooks

--- react-hooks/lessons/use-ref/slides/use-custom-compare.tsx
+++ react-hooks/lessons/use-ref/slides/use-custom-compare.tsx
@@ -2,14 +2,21 @@ import { useLayoutEffect, useReducer, useRef } from "react";
 import { usePrevious } from "./use-previous";
 import { shallowEqual } from "shallow-equal";
 
-function useBodyStyles(styles: React.CSSProperties) {
-  const shouldRunEffect = useRef(0);
-  const previousStyles = usePrevious(styles).current;
+function useCustomCompare<T>(
+  value: T,
+  areEqual: (previous: T, current: T) => boolean
+) {
+  const changeRef = useRef(0);
+  const previousValue = usePrevious(value).current;
 
-  if (previousStyles && !shallowEqual(previousStyles, styles)) {
-    shouldRunEffect.current++;
+  if (changeRef.current === 0 || !areEqual(previousValue as T, value)) {
+    changeRef.current++;
   }
 
+  return changeRef.current;
+}
+
+function useBodyStyles(styles: React.CSSProperties) {
   useLayoutEffect(() => {
     console.log("effect");
     Object.entries(styles).map(([style, value]) => {
@@ -19,7 +26,7 @@ function useBodyStyles(styles: React.CSSProperties) {
         String(value)
       );
     });
-  }, [shouldRunEffect.current]);
+  }, [useCustomCompare(styles, shallowEqual)]);
 }
 
 export function CustomDepsManagementSolution() {
